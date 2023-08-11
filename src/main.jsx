5275b7b8de826fcb970e9b4e9f7a114bfdb53e9c
KatiFerrari <katielidasilvaferrari26@gmail.com>
2023-08-11 14:15:22 -0400

    aula-dia11

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import App from './app.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+  <React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    
+  </Routes>
+  </BrowserRouter>  
   </React.StrictMode>,
 )
