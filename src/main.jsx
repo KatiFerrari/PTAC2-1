5d0447487f91aed16e530b3829d3aedc63efddd2
katiferrari <katielidasilvaferrari26@gmail.com>
2023-08-18 14:18:52 -0400

    atualizado-dia18

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import App from './app.jsx'
-
+import Todo from './Todo/Todo.jsx'
+import Home from './Home/Home.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
   <React.StrictMode>
   <BrowserRouter>
   <Routes>
+    <route path= '/home'element= {<Home/>}></route>
+    <route path= '/todo'element= {<Todo/>}></route>
     
   </Routes>
   </BrowserRouter>  
